54bb4e23a29bbb6d2a8dc726836dc5dbf2cd1d31
San-Ba-Wi <144503132+San-Ba-Wi@users.noreply.github.com>
2024-12-12 16:03:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 # Streamlit 애플리케이션
 st.title("MAL by Jihoon")
 st.write("Mathematics and Algorithm Lab")
-st.image("shimarin.jpg", width=500)  # 이미지 크기를 300픽셀로 조정
+st.image("images/shimarin.jpg", width=500)  # 이미지 크기를 300픽셀로 조정
